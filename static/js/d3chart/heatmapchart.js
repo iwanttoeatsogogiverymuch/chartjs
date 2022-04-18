54c402d900c04128c103621a93ac4212633a5101
iwanttoeatsogogiverymuch <dlawoerjsla@naver.com>
2022-04-18 16:39:16 +0900

    cors problem

--- static/js/d3chart/heatmapchart.js
+++ static/js/d3chart/heatmapchart.js
@@ -43,7 +43,7 @@ window.addEventListener("load", function () {
     var myColor = d3.scaleLinear()
         .range(["white", "#69b3a2"])
         .domain([1, 100])
-
+       
     //Read the data
     d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function (data) {
 
