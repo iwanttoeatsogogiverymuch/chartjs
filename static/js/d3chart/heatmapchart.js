7865d0daa94e5391e9733043b4265c8be151f83a
iwanttoeatsogogiverymuch <dlawoersjla@naver.com>
2022-04-25 18:00:47 +0900

    cohort test

--- static/js/d3chart/heatmapchart.js
+++ static/js/d3chart/heatmapchart.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function () {
 
 
     // Read the data
-    d3.json("/resource/tableConvert.json", function (err,data) {
+    d3.json("/resource/retention.json", function (err,data) {
 
         if(err) {
             console.error("error in heatmap json load");
@@ -16,8 +16,8 @@ window.addEventListener("load", function () {
 
         // set the dimensions and margins of the graph
         var margin5 = { top: 20, right: 20, bottom: 20, left: 20 },
-            width5 = 400 - margin5.left - margin5.right,
-            height5 = 400 - margin5.top - margin5.bottom;
+            width5 = 800 - margin5.left - margin5.right,
+            height5 = 800 - margin5.top - margin5.bottom;
     
         // append the svg object to the body of the page
         var svg6 = d3.select("#my_dataviz")
@@ -25,20 +25,31 @@ window.addEventListener("load", function () {
             .attr("width", width5 + margin5.left + margin5.right)
             .attr("height", height5 + margin5.top + margin5.bottom)
             .attr("viewBox","0 0 800 800")
-            .attr("preserveAspectRatio","none")
+       
             .append("g")
             .attr("transform",
                 "translate(" + margin5.left + "," + margin5.top + ")");
     
         // Labels of row and columns
-        var myGroups = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","11","12","13","14","15"];
-        var heatmapKeys = Object.keys(data); 
-        var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"];
+        var datesettings = moment().format('YYYY-MM-DD');
+
+     //   const cohortDates = Array.from(new d3.InternSet(retentionCohorts.map(d => d.cohort_date))).sort(d3.ascending);
+      //  const periodNumbers = Array.from(new Set(retentionCohorts.map(d => d.period_number))).sort(d3.ascending);
+
+      var signdates = data.map(function(d,i) {
+
+        return d.signdate;
+
+      });
+
+      var retentiondates = data.map(function (d,i) {
+          return d.retentiondate ;
+      });
     
         // Build X scales and axis:
         var x5 = d3.scaleBand()
             .range([0,width5])
-            .domain(myGroups)
+            .domain(retentiondates)
             .padding(0.01);
         svg6.append("g")
             .attr("transform", "translate(0," +0 + ")")
@@ -47,14 +58,14 @@ window.addEventListener("load", function () {
         // Build Y scales and axis:
         var y5 = d3.scaleBand()
             .range([height5, 0])
-            .domain(myVars)
+            .domain(signdates)
             .padding(0.01);
         svg6.append("g")
             .call(d3.axisLeft(y5));
     
         // Build color scale
         var myColor = d3.scaleLinear()
-            .range(["blue", "red"])
+            .range(["blue", "white", "red"])
             .domain([1, 100])
 
 
@@ -65,20 +76,27 @@ window.addEventListener("load", function () {
             .append("rect")
             .attr("x",function ( d, i) 
             { 
-                console.log(d[i]);
-                console.log(x5(parseInt(d[i])));
-                return x5(i);
+    d
+                return x5(d.retentiondate);
             })
             .attr("y", function ( d, i) 
             { 
-                return 30;
+                return y5(d.signdate);
             })
-            .attr("width", 30)
-            .attr("height", 30)
-            .style("fill", "red");
+            .attr("width", function (d){
+                return x5.bandwidth()
+            })
+            .attr("height", function (d){
+                return y5.bandwidth()
+            })
+            .style("fill", function (d){
+                return myColor(d.retentionvalue);
+            });
 
             svg6.append("g")
+            .attr("transform", "translate(0," +0 + ")")
             .selectAll("g")
+            .attr("font-family", "Noto Sans KR")
             .data(data)
             .enter()
             .append("g")
@@ -87,23 +105,24 @@ window.addEventListener("load", function () {
             .enter()
             .append("text")
             .text(function (d){
-                return "ㅇㅇ";
+                return d.retentionvalue + "%";
             })
-            .attr("x", function(d, i, j) {
+            .attr("x", function(d) {
                 // return (i * 20) + 40;
-                return x5(d.group);
+
+                return x5(d.retentiondate);
             })
             .attr("y", function(d) {
-                return y5(d.variable);
+                return y5(d.signdate);
                 //return (local.get(this) * 20) + 40;
             })
             .attr("dx", x5.bandwidth()/2)
             .attr("dy", y5.bandwidth()/2)
             .attr("dominant-baseline", "text-before-edge")
             .attr("text-anchor", "middle")
-            .attr("fill","green")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "12px")
+            .attr("fill","white")
+            .attr("font-weight","Light")
+            .attr("font-size", "2rem")
 
 
     });
