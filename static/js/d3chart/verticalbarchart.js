209fe9f420de2f5e45e5e85cc53578f54e33a10c
iwanttoeatsogogiverymuch <dlawoersjla@naver.com>
2022-04-28 16:21:46 +0900

    vertical bar chart

--- static/js/d3chart/verticalbarchart.js
+++ static/js/d3chart/verticalbarchart.js
@@ -26,12 +26,12 @@ window.addEventListener('load', function () {
 
   var svg2 = d3.select("#verticalbarchart")
     .append("svg")
-    .attr("width", 1920)
-    .attr("height", 400)
-    .attr("viewBox", "0 0 1920 400")
+    .attr("width", 400)
+    .attr("height", 600)
+    .attr("viewBox", "0 0 400 600")
     .attr("preserveAspectRatio", "none");
 
-  var margin2 = { top: 10, right: 30, bottom: 30, left: 40 };
+  var margin2 = { top: 10, right: 70, bottom: 30, left: 40 };
   var width2 = +svg2.attr("width") - margin2.left - margin2.right;
   var height2 = +svg2.attr("height") - margin2.top - margin2.bottom;
 
@@ -40,12 +40,12 @@ window.addEventListener('load', function () {
     .append("g")
     .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-  var x0 = d3.scaleBand()
-    .rangeRound([0, width2]).paddingInner(0.15);
-  var x1 = d3.scaleBand()
+  var verticaly0 = d3.scaleBand()
+    .rangeRound([0, height2]).paddingInner(0.15);
+  var verticaly1 = d3.scaleBand()
     .padding(0.1);
-  var y = d3.scaleLinear()
-    .rangeRound([height2, 0]);
+  var verticalx = d3.scaleLinear()
+    .rangeRound([width2, 0]);
 
 
   var mcgpalette0 = [
@@ -89,15 +89,15 @@ window.addEventListener('load', function () {
     function (error, data) {
       if (error) throw error;
       var keys = data.columns.slice(1);
-      x0.domain(
+      verticaly0.domain(
         data.map(function (d) {
           return d.State;
         })
       );
-      x1.domain(keys)
-        .rangeRound([0, x0.bandwidth()]);
+      verticaly1.domain(keys)
+        .rangeRound([0, verticaly0.bandwidth()]);
 
-      y.domain([
+      verticalx.domain([
         0,
         d3.max(data, function (d) {
           return d3.max(keys, function (key) {
@@ -112,8 +112,7 @@ window.addEventListener('load', function () {
         .enter()
         .append("g")
         .attr("transform", function (d) {
-          return "translate(" + "0," +  x0(d.State);
-        })
+          return "translate(" + "0," +  verticaly0(d.State)+")";})
         .selectAll("rect")
         .data(function (d) {
           return keys.map(function (key) {
@@ -122,17 +121,13 @@ window.addEventListener('load', function () {
         })
         .enter()
         .append("rect")
-        .attr("x", function (d) {
-          return x1(d.key);
-        })
-        .attr("width", x1.bandwidth())
+        .attr("x", verticalx(0))
+        .attr("height", verticaly1.bandwidth())
 
-        .attr("y", y(0)).on("mouseover", function () { tooltip.style("display", null); })
+        .attr("y",function (d) { return verticaly1(d.key);  }).on("mouseover", function () { tooltip.style("display", null); })
         .on("mouseout", function () { tooltip.style("display", "none"); })
         .on("mousemove", function (d) {
 
-          // var subgroupName = d3.select(this.parentNode).datum().key;
-          // var subgroupValue = d.data[subgroupName];
 
           tooltip.style("left", (d3.event.pageX + 10) + "px");
           tooltip.style("top", (d3.event.pageY - 10) + "px");
@@ -144,12 +139,12 @@ window.addEventListener('load', function () {
         .delay(function (d, i) {
           return i * 100;
         }).ease(d3.easeSin)
-        .attr("height", function (d) {
-          return height2 - y(d.value);
+        .attr("width", function (d) {
+          return width2 - verticalx(d.value)  ;
         })
 
-        .attr("y", function (d) {
-          return y(d.value);
+        .attr("x", function (d) {
+          return 0;
         })
         .attr("rx",2)
         .attr("fill", function (d) {
@@ -158,104 +153,104 @@ window.addEventListener('load', function () {
 
 
       //tooltip text top
-      g2.append("g")
-        .selectAll("g")
-        .data(data)
-        .enter()
-        .append("g")
-        .attr("transform", function (d) {
-          return "translate(" + x0(d.State) + ",0)";
-        })
-        .selectAll("text")
-        .data(function (d) {
-          return keys.map(function (key) {
-            return { key: key, value: d[key] };
-          });
-        })
-        .enter()
-        .append("text")
-        .attr("dy", "1em")
-        .attr("fill", "#000")
-        .attr("font-weight", "Regular")
-        .attr("font-family", "Noto Sans KR")
-        .attr("font-size", "1em")
-        .attr("text-anchor", "start")
-        .attr("x", function (d) {
-          return x1(d.key);
-        })
-        .attr("width", x1.bandwidth())
-        .attr("height", function (d) {
-          return height2 - y(d.value);
-        })
-        .attr("y", function (d) {
-          return y(d.value) - 22;
-        })
-        .text(function (d) {
+      // g2.append("g")
+      //   .selectAll("g")
+      //   .data(data)
+      //   .enter()
+      //   .append("g")
+      //   .attr("transform", function (d) {
+      //     return "translate(" + x0(d.State) + ",0)";
+      //   })
+      //   .selectAll("text")
+      //   .data(function (d) {
+      //     return keys.map(function (key) {
+      //       return { key: key, value: d[key] };
+      //     });
+      //   })
+      //   .enter()
+      //   .append("text")
+      //   .attr("dy", "1em")
+      //   .attr("fill", "#000")
+      //   .attr("font-weight", "Regular")
+      //   .attr("font-family", "Noto Sans KR")
+      //   .attr("font-size", "1em")
+      //   .attr("text-anchor", "start")
+      //   .attr("x", function (d) {
+      //     return x1(d.key);
+      //   })
+      //   .attr("width", x1.bandwidth())
+      //   .attr("height", function (d) {
+      //     return height2 - y(d.value);
+      //   })
+      //   .attr("y", function (d) {
+      //     return y(d.value) - 22;
+      //   })
+      //   .text(function (d) {
 
-          if (d.key == "전체") {
-            return setComma(d.value);
-          }
-          else {
-
-
-          }
+      //     if (d.key == "전체") {
+      //       return setComma(d.value);
+      //     }
+      //     else {
 
 
-        });
+      //     }
+
+
+      //   });
 
 
 
 
-      g2.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + height2 + ")")
-        .call(d3.axisBottom(x0))
-        .call(function (g) { g.selectAll(".tick line").remove() })
-        .call(function (g) { g.selectAll(".domain").attr("stroke-width", "5").attr("stroke-opacity", "0.5") })
-        .call(function (g) { g.selectAll("text").attr("font-family", "Noto Sans KR").attr("fill", "grey") });
+      // g2.append("g")
+      //   .attr("class", "axis")
+      //   .attr("transform", "translate(0," + height2 + ")")
+      //   .call(d3.axisBottom(verticalx))
+      //   .call(function (g) { g.selectAll(".tick line").remove() })
+      //   .call(function (g) { g.selectAll(".domain").attr("stroke-width", "5").attr("stroke-opacity", "0.5") })
+      //   .call(function (g) { g.selectAll("text").attr("font-family", "Noto Sans KR").attr("fill", "grey") });
 
-      g2.append("g")
-        .attr("class", "axis")
-        .call(d3.axisLeft(y).ticks(null, "s"))
-        .append("text")
-        .attr("x", width2 / 2)
-        .attr("y", y(y.ticks().pop()) + 0.5)
-        .attr("dy", "0.32em")
-        .attr("fill", "#000")
-        .attr("font-weight", "Regular")
-        .attr("font-family", "Noto Sans KR")
-        .attr("font-size", "0.2em")
-        .attr("text-anchor", "middle");
-
-
-      var legend2 = g2
-        .append("g")
-        .attr("font-family", "Noto Sans KR")
-        .attr("font-size", "1em")
-        .attr("text-anchor", "end")
-        .selectAll("g")
-        .data(keys.slice().reverse())
-        .enter()
-        .append("g")
-        .attr("transform", function (d, i) {
-          return "translate(0," + i * 20 + ")";
-        });
+      // g2.append("g")
+      //   .attr("class", "axis")
+      //   .call(d3.axisLeft(verticaly1).ticks(null, "s"))
+      //   .append("text")
+      //   .attr("x", 300 )
+      //   .attr("y", height2 / 2 )
+      //   .attr("dy", "0.32em")
+      //   .attr("fill", "#000")
+      //   .attr("font-weight", "Regular")
+      //   .attr("font-family", "Noto Sans KR")
+      //   .attr("font-size", "0.2em")
+      //   .attr("text-anchor", "middle");
 
 
-      legend2
-        .append("rect")
-        .attr("x", width2 - 19)
-        .attr("width", 19)
-        .attr("height", 19)
-        .attr("fill", z2);
+      // var legend2 = g2
+      //   .append("g")
+      //   .attr("font-family", "Noto Sans KR")
+      //   .attr("font-size", "1em")
+      //   .attr("text-anchor", "end")
+      //   .selectAll("g")
+      //   .data(keys.slice().reverse())
+      //   .enter()
+      //   .append("g")
+      //   .attr("transform", function (d, i) {
+      //     return "translate(0," + i * 20 + ")";
+      //   });
 
-      legend2
-        .append("text")
-        .attr("x", width2 - 24)
-        .attr("y", 13)
-        .text(function (d) {
-          return d;
-        });
+
+      // legend2
+      //   .append("rect")
+      //   .attr("x", width2 - 19)
+      //   .attr("width", 19)
+      //   .attr("height", 19)
+      //   .attr("fill", z2);
+
+      // legend2
+      //   .append("text")
+      //   .attr("x", width2 - 24)
+      //   .attr("y", 13)
+      //   .text(function (d) {
+      //     return d;
+      //   });
 
 
 
